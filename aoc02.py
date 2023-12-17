5fc629827a706643a8733635a2326dd620920e48
jawaadb <86400666+jawaadb@users.noreply.github.com>
2023-12-17 15:02:37 +0000

    AOC 2 solved all parts

--- aoc02.py
+++ aoc02.py
@@ -53,14 +53,39 @@ def sum_of_valid_game_ids(lines: list[str]):
     return id_sum
 
 
+
+def calc_power(gset: list[str]) -> int:
+    max_count = np.zeros(len(CUBE_COLOURS), dtype=np.int64)
+
+    for r in gset:
+        for c in split_result(r):
+            idx = CUBE_COLOURS.index(c[1])
+            count = c[0]
+            max_count[idx] = max(max_count[idx], count)
+
+    return np.prod(max_count)
+
+
+def calc_power_sum(lines: list[str]) -> int:
+    sum = 0
+    for l in lines:
+        sum += calc_power(split_game(l)[1])
+
+    return sum
+
+
 def main():
     lines = list(map(str.strip, load_text(EXAMPLE_1_FILE)))
     assert sum_of_valid_game_ids(lines) == 8
 
     lines = list(map(str.strip, load_text(PROBLEM_1_FILE)))
-    result = sum_of_valid_game_ids(lines)
+    assert sum_of_valid_game_ids(lines) == 2416
 
-    print(f"{result=}")
+    lines = list(map(str.strip, load_text(EXAMPLE_1_FILE)))
+    assert calc_power_sum(lines)==2286
+
+    lines = list(map(str.strip, load_text(PROBLEM_1_FILE)))
+    print(f"result={calc_power_sum(lines)}")
 
 
 if __name__ == '__main__':
