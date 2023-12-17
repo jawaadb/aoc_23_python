7d4aa4a0525a06d0a1e5f1e1e00db8f8fc693604
jawaadb <86400666+jawaadb@users.noreply.github.com>
2023-12-17 08:48:47 +0000

    AOC1 part 1 done

--- aoc01.py
+++ aoc01.py
@@ -1,21 +1,26 @@
 EXAMPLE_FILE = "data/aoc01_example.txt"
-
+PROBLEM_FILE_1 = "data/aoc01_problem1.txt"
 
 def load_text(fpath):
     with open(fpath, 'r', encoding='utf-8') as f:
         return f.readlines()
 
 
-def main():
+def compute_answer(fpath: str):
     result: int = 0
 
-    for line in load_text(EXAMPLE_FILE):
+    for line in load_text(fpath):
         num_chars = [char for char in line if char.isdigit()]
         assert len(num_chars) != 0
 
         result += int(num_chars[0] + num_chars[-1])
 
-    print(result)
+    return result
+
+
+def main():
+    print(compute_answer(EXAMPLE_FILE))
+    print(compute_answer(PROBLEM_FILE_1))
 
 
 if __name__ == '__main__':
