5726600b5b289725b481bcf4baa77a72d7827029
jawaadb <86400666+jawaadb@users.noreply.github.com>
2023-12-17 23:43:12 +0000

    AOC3 part 1 solved

--- aoc03.py
+++ aoc03.py
@@ -1,4 +1,4 @@
-from helpers import load_text
+from helpers import load_text, get_answer
 import numpy as np
 
 EXAMPLE_1_FILE = "data/aoc03/example1.txt"
@@ -46,32 +46,42 @@ def is_symbol(c: str):
         return True
 
 
-def is_near_symbol(num: tuple[int, int, str], arr: np.ndarray):
+def calc_symbols_mask(arr: np.ndarray):
     mask_symbol = np.zeros(arr.shape, dtype=np.bool_)
     for i in range(arr.shape[0]):
         for j in range(arr.shape[1]):
             mask_symbol[i, j] = is_symbol(arr[i, j])
+    return mask_symbol
+
+
+def is_near_symbol(num: tuple[int, int, str], arr: np.ndarray, sym_mask: np.ndarray):
+    assert arr.shape == sym_mask.shape
 
     res: bool = False
 
     i = num[0]
     for j in range(num[1], num[1]+len(num[2])):
-        mask_sub = mask_symbol[(i-1):(i+2), (j-1):(j+2)]
+        mask_sub = sym_mask[(i-1):(i+2), (j-1):(j+2)]
         res |= np.any(mask_sub)
 
     return res
 
 
-def main():
-    lines = list(map(str.strip, load_text(EXAMPLE_1_FILE)))
-
+def calc_part_nums_sum(lines: list[str]):
     arr = expand_mat(np.array([[c for c in l] for l in lines], '<U1'))
-
-    print(arr)
+    sym_mask = calc_symbols_mask(arr)
 
     part_nums = [int(num[2])
-                 for num in find_numbers(arr) if is_near_symbol(num, arr)]
-    print(np.sum(part_nums))
+                 for num in find_numbers(arr) if is_near_symbol(num, arr, sym_mask)]
+    return np.sum(part_nums)
+
+
+def main():
+    lines = list(map(str.strip, load_text(EXAMPLE_1_FILE)))
+    assert calc_part_nums_sum(lines) == int(get_answer(3, 1))
+
+    lines = list(map(str.strip, load_text(PROBLEM_1_FILE)))
+    assert calc_part_nums_sum(lines) == int(get_answer(3, 2))
 
 
 if __name__ == '__main__':
